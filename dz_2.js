5fe0d7a9e42cdf871d6908deabe5bb8e0c9c77e7
Arhip_ops <harkhip777@gmail.com>
2025-09-27 08:29:04 +0300

    dz2

--- dz_2.js
+++ dz_2.js
@@ -4,5 +4,10 @@ function getDate() {
   const date = moment().format("YYYY/DD/MM HH:mm:ss");
   console.log(date);
 }
+function getCurrentWeekday() {
+  const weekday = moment().format("dddd"); 
+  console.log( weekday);
+}
 
 getDate();
+getCurrentWeekday();
