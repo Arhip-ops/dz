0d96af7558cb2fe9709aaf75d3f4ec7b3835a870
Arhip_ops <harkhip777@gmail.com>
2025-09-27 08:32:28 +0300

    dz3

--- dz_2.js
+++ dz_2.js
@@ -1,13 +1,31 @@
-let moment = require("moment");
+const moment = require("moment");
+const express = require("express")
+
+const app = express();
+const PORT = 8000;
 
 function getDate() {
   const date = moment().format("YYYY/DD/MM HH:mm:ss");
   console.log(date);
+  return moment().format("YYYY/MM/DD HH:mm:ss");
 }
+
 function getCurrentWeekday() {
   const weekday = moment().format("dddd"); 
   console.log( weekday);
+  return moment().format("dddd");
 }
 
 getDate();
 getCurrentWeekday();
+
+app.get("/timestamp", (req, res) => {
+  res.json({
+    timestamp: getDate(),
+    weekday: getCurrentWeekday(),
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Сервер запущен`);
+});
