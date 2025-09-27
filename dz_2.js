58ebc09352ff2a3ee5bee5c1d5f40246c487224a
Arhip_ops <harkhip777@gmail.com>
2025-09-27 08:45:10 +0300

    dz5

--- dz_2.js
+++ dz_2.js
@@ -23,13 +23,57 @@ app.get("/timestamp", (req, res) => {
 
 app.get("/posts", (req, res) => {
   const filePath = path.join(__dirname, "posts.json");
-    
-  try {
-      const posts = JSON.parse(data);
-      res.json(posts);
+
+    let posts;
+    try {
+      posts = JSON.parse(data);
     } catch {
-      res.status(500).json({ error: "error" });
+      return res.status(500).json({ error: "error" });
     }
+
+    const skip = req.query.skip ? Number(req.query.skip) : 0;
+    const take = req.query.take ? Number(req.query.take) : null;
+
+    if ((req.query.skip && !Number.isInteger(skip)) || (req.query.take && !Number.isInteger(take))) {
+      return res.status(400).json({ error: "skip и take должны быть числами" });
+    }
+
+    let result = posts;
+
+    if (skip > 0) {
+      result = result.slice(skip);
+    }
+    if (take !== null) {
+      result = result.slice(0, take);
+    }
+
+    res.json(result);
+  });
+
+
+app.get("/posts/:id", (req, res) => {
+  const filePath = path.join(__dirname, "posts.json");
+
+    let posts;
+    try {
+      posts = JSON.parse(data);
+    } catch {
+      return res.status(500).json({ error: "error" });
+    }
+
+    const id = Number(req.params.id);
+
+    if (!Number.isInteger(id)) {
+      return res.status(400).json({ error: "ID должен быть числом" });
+    }
+
+    const post = posts.find(p => p.id === id);
+
+    if (!post) {
+      return res.status(404).json({ error: "Пост не найден" });
+    }
+
+    res.json(post);
   });
 
 
